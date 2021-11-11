702a890b7c3414c97bf388976414ce20cac2bad3
phanthianhthu <thuptagcc19106@fpt.edu.vn>
2021-11-11 15:59:27 +0700

    Update content.php

--- Shopping/content.php
+++ Shopping/content.php
@@ -8,7 +8,7 @@ include_once("connection.php");
 			<div class="block-slider block-slider4">
 				<ul class="" id="bxslider-home4">
 					<li>
-						<img src="images/anhnen.jpg" alt="Slide">
+						<img src="images/hinhnen1.jpg" alt="Slide">
 						<div class="caption-group"></div>
 					</li>
                 </ul>
@@ -30,24 +30,24 @@ include_once("connection.php");
                         <!--Load san pham tu DB -->
                            <?php
 						  // 	include_once("database.php");
-		  				   	$result = mysqli_query($conn, "SELECT * FROM product" );
+		  				   	$result = pg_query($conn, "SELECT * FROM product" );
 			
-			                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+			                while($row = pg_fetch_array($result, NULL, PGSQL_ASSOC)){
 				            ?>
 				            <!--Một sản phẩm -->
                             <div class="single-product">
                                 <div class="product-f-image">
-                                    <img src="images/<?php echo $row['Pro_image']?>" width="150" height="150">
+                                    <img src="images/<?php echo $row['pro_image']?>" width="150" height="150">
                                     <div class="product-hover">
-                                        <a href="?page=cart&ma=<?php echo  $row['Product_ID']?>" class="add-to-cart-link" ><i class="fa fa-shopping-cart"></i> Add to cart</a>
-                                        <a href="?page=menu&ma=<?php echo  $row['Product_ID']?>" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                        <a href="?page=cart&ma=<?php echo  $row['product_id']?>" class="add-to-cart-link" ><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="?page=menu&ma=<?php echo  $row['product_id']?>" class="view-details-link"><i class="fa fa-link"></i> See details</a>
                                     </div>
                                 </div>
                                 
-                                <h2><a href="?page=menu&ma=<?php echo  $row['Product_ID']?>">Product Name: <?php echo  $row['Product_Name']?></a></h2>
+                                <h2><a href="?page=menu&ma=<?php echo  $row['product_id']?>">Product Name: <?php echo  $row['product_name']?></a></h2>
                                 
                                 <div class="product-carousel-price">
-                                    <ins>Price($): <?php echo  $row['Price']?></ins>
+                                    <ins>Price($): <?php echo  $row['price']?></ins>
                                 </div> 
                             </div>
                 
